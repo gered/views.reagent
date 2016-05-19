6563a3ecc473ae654a22f9b19599d27bc1e98b5c
gered <gered@blarg.ca>
2016-05-19 13:40:57 -0400

    begin reorganizing project layout

---
+++ reagent-data-views/project.clj
@@ -0,0 +1,14 @@
+(defproject reagent-data-views "0.2.0-SNAPSHOT"
+  :description  "Support for Reagent components that get pushed realtime database updates from the server."
+  :url          "https://github.com/gered/reagent-data-views"
+  :license      {:name "MIT License"
+                 :url  "http://opensource.org/licenses/MIT"}
+
+  :dependencies []
+
+  :profiles     {:provided
+                 {:dependencies
+                  [[org.clojure/clojure "1.8.0"]
+                   [org.clojure/clojurescript "1.8.51"]
+                   [reagent "0.6.0-alpha"]
+                   [gered/views "1.5-SNAPSHOT"]]}})

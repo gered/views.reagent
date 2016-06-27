481ae19d8788faf577f64c245597b258790ba64f
gered <gered@blarg.ca>
2016-06-27 18:46:45 -0400

    views.reagent - version 0.1
    rewinding the version back down to 0.1 as it seemed somewhat weird to
    me to have it at v0.2.1 even though i really feel like this is the
    first actual *usable* release (and the project was also renamed some
    where along the line too)

--- views.reagent/project.clj
+++ views.reagent/project.clj
@@ -1,4 +1,4 @@
-(defproject gered/views.reagent "0.2.1-SNAPSHOT"
+(defproject gered/views.reagent "0.1"
   :description  "Reagent plugin for the views library, providing real-time component updates to server-side changes to data."
   :url          "https://github.com/gered/views.reagent"
   :license      {:name "MIT License"
@@ -13,7 +13,7 @@
                   [[org.clojure/clojure "1.8.0"]
                    [org.clojure/clojurescript "1.8.51"]
                    [reagent "0.6.0-alpha"]
-                   [gered/views "1.5-SNAPSHOT"]]}}
+                   [gered/views "1.5"]]}}
 
   :cljsbuild    {:builds
                  {:main

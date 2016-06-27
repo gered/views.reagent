bad0d977eece97456f0001d9112eb515ad858391
gered <gered@blarg.ca>
2016-06-27 18:53:29 -0400

    bump views.reagent.browserchannel version for development

--- views.reagent.browserchannel/project.clj
+++ views.reagent.browserchannel/project.clj
@@ -1,4 +1,4 @@
-(defproject gered/views.reagent.browserchannel "0.1"
+(defproject gered/views.reagent.browserchannel "0.2-SNAPSHOT"
   :description  "BrowserChannel client/server messaging adapter for views.reagent."
   :url          "https://github.com/gered/views.reagent"
   :license      {:name "MIT License"

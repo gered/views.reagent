948865a0d72d4f12924f6f3c69dd0f3d8b76e938
gered <gered@blarg.ca>
2016-06-27 18:49:56 -0400

    views.reagent.sente - version 0.1

--- views.reagent.sente/project.clj
+++ views.reagent.sente/project.clj
@@ -1,4 +1,4 @@
-(defproject gered/views.reagent.sente "0.1.0-SNAPSHOT"
+(defproject gered/views.reagent.sente "0.1"
   :description  "Sente client/server messaging adapter for views.reagent."
   :url          "https://github.com/gered/views.reagent"
   :license      {:name "MIT License"
@@ -13,8 +13,8 @@
                   [[org.clojure/clojure "1.8.0"]
                    [org.clojure/clojurescript "1.8.51"]
                    [reagent "0.6.0-alpha"]
-                   [gered/views "1.5-SNAPSHOT"]
-                   [gered/views.reagent "0.2.0-SNAPSHOT"]
+                   [gered/views "1.5"]
+                   [gered/views.reagent "0.1"]
                    [com.taoensso/sente "1.8.1"]]}}
 
   :cljsbuild    {:builds
